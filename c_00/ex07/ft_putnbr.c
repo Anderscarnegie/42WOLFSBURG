697adb9fc58ca15adc85ef912966227c235547ec
Igors78 <oleinikovsigors@gmail.com>
2021-02-17 19:13:24 +0200

    Add c_02_ex10

--- c_00/ex07/ft_putnbr.c
+++ c_00/ex07/ft_putnbr.c
@@ -7,18 +7,21 @@ void ft_putchar(char c)
 
 void ft_putnbr(int nb)
 {
-    if (nb < 0)
+    long x;
+
+    x = nb;
+    if (x < 0)
     {
-        nb = nb * (-1);
+        x = x * (-1);
         ft_putchar('-');
     }
-    if (nb >= 10)
+    if (x >= 10)
     {
-        ft_putnbr(nb / 10);
-        ft_putnbr(nb % 10);
+        ft_putnbr(x / 10);
+        ft_putnbr(x % 10);
     }
     else
     {
-        ft_putchar(nb + '0');
+        ft_putchar(x + '0');
     }
 }
---
+++ c_02/ex10/ft_strlcpy.c
@@ -0,0 +1,22 @@
+unsigned int ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+    unsigned int i;
+    int srclength;
+
+    i = 0;
+    srclength = 0;
+    if (size > 0)
+    {
+        while (src[i] != '\0' && i < size)
+        {
+            dest[i] = src[i];
+            i++;
+        }
+        dest[i] = '\0';
+    }
+    while (src[i] != '\0')
+    {
+        srclength++;
+    }
+    return (srclength);
+}
