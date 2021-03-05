689366fc3304fa3c1a22a36916d843ad485d1304
Igors78 <oleinikovsigors@gmail.com>
2021-03-05 12:22:25 +0200

    Add cached fibonacci

--- C_practice/cache_fibonacci/cache_fib.c
+++ C_practice/cache_fibonacci/cache_fib.c
@@ -30,21 +30,21 @@ void ft_putnbr(int nb)
 
 int fib(int n)
 {
-    if (n <= 1)
-        return n;
-    if (cache[n] != 0)
-        return cache[n];
-    else
-    {
+    if (n < 0)
+        return (-1);
+    else if (n == 0)
+        return (0);
+    else if (n == 1)
+        return (1);
+    else if (cache[n] == 0)
         cache[n] = fib(n - 1) + fib(n - 2);
-        return cache[n];
-    }
+    return cache[n];
 }
 
 int main(void)
 {
     int res;
-    res = fib(55);
+    res = fib(47);
     ft_putnbr(res);
     ft_putchar('\n');
 }
