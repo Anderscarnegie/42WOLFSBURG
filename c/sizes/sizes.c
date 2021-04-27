9119a6f94dfb5233cd871ef3752ce45d44dc4c39
Igors78 <oleinikovsigors@gmail.com>
2021-04-27 17:51:58 +0300

    Try fprintf

--- c/sizes/sizes.c
+++ c/sizes/sizes.c
@@ -6,6 +6,7 @@ int main(void)
     int x = 10;
     int *p = &x;
 
+    fprintf(stdout, "Sizes of some common data types in current compiler\n");
     printf("Size of int is %d bytes\n", (int)sizeof(int));
     printf("Size of unsigned int is %d bytes\n", (int)sizeof(unsigned int));
     printf("Size of char is %d bytes\n", (int)sizeof(char));
@@ -15,6 +16,6 @@ int main(void)
     printf("Size of long long is %d bytes\n", (int)sizeof(long long));
     printf("Size of pointer is %d bytes\n", (int)sizeof(p));
     printf("Size of short is %d bytes\n", (int)sizeof(short));
-    printf("Size of unsigned short is %d bytes\n", (int)sizeof(unsigned short));
+    fprintf(stdout, "Size of unsigned short is %d bytes\n", (int)sizeof(unsigned short));
     return (0);
 }
