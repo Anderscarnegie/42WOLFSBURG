c4c9b0e988aec76ac8d93e3effc7ef107f29554c
Igors78 <oleinikovsigors@gmail.com>
2021-04-30 12:36:58 +0300

    Add pointer arithmetic example

---
+++ c/arithmetic/arithmetic.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int stringLength(const char *string);
+
+int main()
+{
+    printf("%d \n", stringLength("string Length test"));
+    printf("%d \n", stringLength(""));
+    printf("%d \n", stringLength("igor"));
+
+    return 0;
+}
+
+int stringLength(const char *string)
+{
+    const char *lastAddress = string;
+
+    while (*lastAddress)
+        ++lastAddress;
+
+    return lastAddress - string;
+}
