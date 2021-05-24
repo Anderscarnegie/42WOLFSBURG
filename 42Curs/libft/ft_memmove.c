195662ff349c30f0b77f77b7746c9ef41573472a
Igors78 <oleinikovsigors@gmail.com>
2021-05-24 17:46:46 +0300

    Correct norminette error

--- 42Curs/libft/ft_memmove.c
+++ 42Curs/libft/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: ioleinik <ioleinik@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/12 14:02:42 by ioleinik          #+#    #+#             */
-/*   Updated: 2021/05/12 16:30:11 by ioleinik         ###   ########.fr       */
+/*   Updated: 2021/05/23 17:14:09 by ioleinik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ void	*ft_memmove(void *str1, const void *str2, size_t count)
 	{
 		while (count > 0)
 		{
-			s1[count -1] = s2[count -1];
+			s1[count - 1] = s2[count - 1];
 			count--;
 		}
 	}
