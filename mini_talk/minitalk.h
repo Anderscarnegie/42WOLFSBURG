d6c825e9d6ee84c110f3ea78cb4c158754528135
Igors78 <oleinikovsigors@gmail.com>
2021-07-10 23:32:49 +0300

    Add minitalk

---
+++ mini_talk/minitalk.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ioleinik <ioleinik@student.42wolfsburg.de> +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/04 17:41:43 by ioleinik          #+#    #+#             */
+/*   Updated: 2021/07/10 18:56:08 by ioleinik         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include "includes/libft.h"
+# include <signal.h>
+
+void	terminate(int pid);
+
+#endif
